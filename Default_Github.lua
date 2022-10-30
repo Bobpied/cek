3d61dfafc12a3703488cdd08a310e67f7e13dd82
Bobpied <116987412+Bobpied@users.noreply.github.com>
2022-10-30 12:52:11 +0700

    Add files via upload

---
+++ Default_Github.lua
@@ -0,0 +1,54 @@
+Rdp = {
+    {nordp = 1, botname = "6U1DWx2u", Status = "aktif"},
+    {nordp = 1, botname = "hNQ3wkTv", Status = "aktif"},
+    {nordp = 2, botname = "1X1BOB6PIED", Status = "aktif"},
+    {nordp = 2, botname = "1X1BOB7PIED", Status = "aktif"},
+    {nordp = 2, botname = "1X1BOB8PIED", Status = "aktif"},
+    {nordp = 3, botname = "1X1BOB9PIED", Status = "aktif"},
+    {nordp = 3, botname = "Cxvn4Lxd", Status = "aktif"},
+    {nordp = 3, botname = "bZ1Pc0Md", Status = "aktif"},
+    {nordp = 4, botname = "j10qHSgY", Status = "aktif"},
+    {nordp = 4, botname = "CenAyPGY", Status = "aktif"},
+    {nordp = 4, botname = "IEyY0lwT", Status = "aktif"},
+    {nordp = 5, botname = "aiWG6ZK1", Status = "aktif"},
+    {nordp = 5, botname = "cioYbFkA", Status = "aktif"},
+    {nordp = 5, botname = "rFEqIJmY", Status = "aktif"},
+    {nordp = 6, botname = "sAs8rekT", Status = "aktif"},
+    {nordp = 6, botname = "BOuCJmXE", Status = "aktif"},
+    {nordp = 6, botname = "jp6sqr9g", Status = "aktif"},
+    {nordp = 7, botname = "5qkeOsgV", Status = "aktif"},
+    {nordp = 7, botname = "H6gyuOWZ", Status = "aktif"},
+    {nordp = 7, botname = "DXGBajxj", Status = "aktif"},
+    {nordp = 8, botname = "QgG9vFgV", Status = "aktif"},
+    {nordp = 8, botname = "JFHO12vm", Status = "aktif"},
+    {nordp = 8, botname = "WPY2VUeE", Status = "aktif"},
+    {nordp = 9, botname = "KAD9kp3g", Status = "aktif"},
+    {nordp = 9, botname = "papzUHgj", Status = "aktif"},
+    {nordp = 9, botname = "BFRlqI7E", Status = "aktif"},
+    {nordp = 10, botname = "l35JY5zC", Status = "aktif"},
+    {nordp = 10, botname = "1X1BOB4PIED", Status = "aktif"}
+}
+
+ShowIP = true
+ip_add = {
+    "195.206.235.245",
+    "195.206.235.246",
+    "195.206.235.247",
+    "195.206.235.248",
+    "195.206.235.249",
+    "195.206.235.250",
+    "195.206.235.251",
+    "195.206.235.252",
+    "195.206.235.253",
+    "49.51.28.167"
+}
+
+hookURL = "https://discord.com/api/webhooks/1024803945782198333/ArHCBGrv4pupF6tT5_uwEzxL_qQ2-IMypMCMnBMXZY4iiHJJrD3Yv0uW6X05eJQHzFn7"
+
+Teks_Deskripsi = "List Nick Para Babu"
+Teks_Footer = "Golek Duit Kanggo Masa Depan"
+
+Emote_Offline = "<:offline:982957367392088124>"
+Emote_Online = "<:online:982957403756716052>"
+
+menit = 5 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
